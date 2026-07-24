500276620db5130744637d04907d59dca3da33c8
Yagami <mvinicosta1103@gmail.com>
2026-07-24 18:19:39 -0300

    V1

--- src/main/java/dev/yagami/elementalsmorebendings/morebendings/elements/EarthSubbendings/crystal/CrystalBending.java
+++
@@ -1,9 +0,0 @@
-package com.elementals.morebendings.bending.crystal;
-
-public class CrystalBending {
-    public static final String SUBBENDING_NAME = "Crystal Bending";
-
-    public static boolean isCrystalBender(Object player) {
-        return true;
-    }
-}
